bdd3ec664dc9713b13be1ae8f1d2e8d090a636a3
Maria Lapshina <mariaspiiish@gmail.com>
2023-04-01 17:31:39 +0400

    validation

--- practice-1/index.js
+++ practice-1/index.js
@@ -1,7 +1,17 @@
 import Popup from './components/Popup.js';
+import FormValidator from './components/FormValidator.js';
 
 const popup = document.querySelector('.popup');
 const button = document.querySelector('.button');
+const formElement = document.querySelector('.form');
+const selectorList = {
+    formSelector: '.form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__submit-button',
+    inactiveButtonClass: 'popup__submit-button_inactive',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__input-error_active',
+  }
 
 const form = new Popup({
     handleFormSubmit: (data) => {
@@ -13,7 +23,10 @@ const form = new Popup({
 
 form.setEventListeners();
 
+const formValidator = new FormValidator(selectorList, formElement);
+formValidator.enableValidation();
+
 button.addEventListener('click', () => {
-    // formValidatorProfile.resetValidation();
+    formValidator.resetValidation();
     form.open();
 })
